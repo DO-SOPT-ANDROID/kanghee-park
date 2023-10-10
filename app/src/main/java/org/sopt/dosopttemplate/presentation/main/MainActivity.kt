f0a565cbebc8f286e9785cbe6d08218bdb22e1c3
Hailey <91793891+stellar-halo@users.noreply.github.com>
2023-10-11 02:50:09 +0900

    [feat] login에서 사용자 정보 받아오기

--- app/src/main/java/org/sopt/dosopttemplate/presentation/main/MainActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/main/MainActivity.kt
@@ -1,12 +1,27 @@
 package org.sopt.dosopttemplate.presentation.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.data.UserInfo
+import org.sopt.dosopttemplate.databinding.ActivityMainBinding
+import org.sopt.dosopttemplate.presentation.signUp.SignUpActivity.Companion.USER_INFO
+import org.sopt.dosopttemplate.util.binding.BindingActivity
+import org.sopt.dosopttemplate.util.extensions.getParcelable
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding.viewModel = viewModel
+
+        getUserInfo()
+    }
+
+    private fun getUserInfo() {
+        val userInfo = intent.getParcelable(USER_INFO, UserInfo::class.java)
+        if (userInfo != null) {
+            viewModel.setUserInfo(userInfo)
+        }
     }
 }
