02142a16b3e67d83eafff1e27857b3540a49834c
Hailey <91793891+stellar-halo@users.noreply.github.com>
2023-10-11 00:40:35 +0900

    [feat] bindingActivity로 변경 및 Main으로 이동 함수

--- app/src/main/java/org/sopt/dosopttemplate/presentation/login/LoginActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/login/LoginActivity.kt
@@ -3,23 +3,28 @@ package org.sopt.dosopttemplate.presentation.login
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import com.google.android.material.snackbar.Snackbar
+import org.sopt.dosopttemplate.R
 import org.sopt.dosopttemplate.data.UserInfo
 import org.sopt.dosopttemplate.databinding.ActivityLoginBinding
+import org.sopt.dosopttemplate.presentation.login.LoginViewModel.Companion.LOGIN_FAILED
+import org.sopt.dosopttemplate.presentation.login.LoginViewModel.Companion.LOGIN_SUCCEED
+import org.sopt.dosopttemplate.presentation.login.LoginViewModel.Companion.NON_MEMBER
+import org.sopt.dosopttemplate.presentation.main.MainActivity
 import org.sopt.dosopttemplate.presentation.signUp.SignUpActivity
 import org.sopt.dosopttemplate.presentation.signUp.SignUpActivity.Companion.USER_INFO
+import org.sopt.dosopttemplate.util.binding.BindingActivity
 
-class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
-
+    private val viewModel by viewModels<LoginViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding.viewModel = viewModel
 
         initActivityLauncher()
         initBtnClickListener()
@@ -31,20 +36,42 @@ class LoginActivity : AppCompatActivity() {
                 if (result.resultCode != RESULT_OK) return@registerForActivityResult
                 val data = result.data ?: return@registerForActivityResult
                 val userInfo = getParcelable(data)
+                viewModel.updateUserInfo(userInfo!!)
             }
     }
 
     private fun initBtnClickListener() {
         binding.btnLoginLogin.setOnClickListener {
-//            TODO("회원가입 확인")
-//            TODO("아이디, 비밀번호 맞는지 확인")
-//            TODO("마이페이지로 넘어가기")
+            login()
         }
         binding.btnLoginSignUp.setOnClickListener {
-            if (::resultLauncher.isInitialized) resultLauncher.launch(
-                Intent(this, SignUpActivity::class.java)
-            )
+            goToSignUp()
         }
+    }
+
+    private fun goToSignUp() {
+        if (::resultLauncher.isInitialized) resultLauncher.launch(
+            Intent(this, SignUpActivity::class.java)
+        )
+    }
+
+    private fun login() {
+        when (viewModel.checkSignUp()) {
+            LOGIN_SUCCEED -> {
+                makeToast(LOGIN_SUCCEED)
+                goToMain()
+            }
+
+            LOGIN_FAILED -> snackBar(LOGIN_FAILED)
+            NON_MEMBER -> snackBar(NON_MEMBER)
+        }
+    }
+
+    private fun goToMain() {
+        val intentToMain = Intent(this, MainActivity::class.java)
+        intent.putExtra(USER_INFO, viewModel.userInfo)
+        startActivity(intentToMain)
+        finish()
     }
 
     private fun getParcelable(data: Intent) =
@@ -53,4 +80,15 @@ class LoginActivity : AppCompatActivity() {
         } else {
             data.getParcelableExtra(USER_INFO)
         }
+
+    private fun snackBar(text: String) {
+        Snackbar.make(
+            binding.root,
+            text,
+            Snackbar.LENGTH_SHORT,
+        ).show()
+    }
+    private fun makeToast(text: String) {
+        Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show()
+    }
 }
