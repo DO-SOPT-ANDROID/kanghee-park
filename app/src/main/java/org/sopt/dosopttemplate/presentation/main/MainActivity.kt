539aa94ac4ef7bba9a152143e9c2be563e3d5e26
Hailey <91793891+stellar-halo@users.noreply.github.com>
2023-10-21 19:23:45 +0900

    [fix] userInfo viewModel로 보내지 않고 fragment로 전송 및 bottomnavigation 연결

--- app/src/main/java/org/sopt/dosopttemplate/presentation/main/MainActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/main/MainActivity.kt
@@ -2,23 +2,71 @@ package org.sopt.dosopttemplate.presentation.main
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
 import org.sopt.dosopttemplate.R
 import org.sopt.dosopttemplate.data.UserInfo
 import org.sopt.dosopttemplate.databinding.ActivityMainBinding
-import org.sopt.dosopttemplate.presentation.signUp.SignUpActivity.Companion.USER_INFO
+import org.sopt.dosopttemplate.presentation.home.HomeFragment
+import org.sopt.dosopttemplate.presentation.myPage.MyPageFragment
+import org.sopt.dosopttemplate.presentation.search.SearchFragment
+import org.sopt.dosopttemplate.presentation.signUp.SignUpActivity
 import org.sopt.dosopttemplate.util.binding.BindingActivity
 import org.sopt.dosopttemplate.util.extensions.getParcelable
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     private val viewModel by viewModels<MainViewModel>()
+    private lateinit var userInfo: UserInfo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.viewModel = viewModel
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_home)
+        if (currentFragment == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fcv_home, HomeFragment())
+                .commit()
+        }
         getUserInfo()
+        clickBottomNavigation()
     }
 
     private fun getUserInfo() {
-        val userInfo = intent.getParcelable(USER_INFO, UserInfo::class.java)?: throw IllegalArgumentException("missing userinfo")
-        viewModel.setUserInfo(userInfo)
+        userInfo = intent.getParcelable(SignUpActivity.USER_INFO, UserInfo::class.java)
+            ?: throw IllegalArgumentException("missing userinfo")
+    }
+
+    private fun clickBottomNavigation() {
+        binding.bnvHome.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+
+                R.id.menu_search -> {
+                    replaceFragment(SearchFragment())
+                    true
+                }
+
+                R.id.menu_mypage -> {
+                    replaceFragment(
+                        MyPageFragment.newInstance(
+                            userInfo.id,
+                            userInfo.password,
+                            userInfo.nickname,
+                            userInfo.mbti
+                        )
+                    )
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_home, fragment)
+            .commit()
     }
 }
