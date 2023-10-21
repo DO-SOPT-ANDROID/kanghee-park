2057535b776f30d5a883e0f26cb12317ff28d5fc
Hailey <91793891+stellar-halo@users.noreply.github.com>
2023-10-21 19:24:28 +0900

    [fix] MainViewModel 로직 삭제 -> 추후 사용 예정

--- app/src/main/java/org/sopt/dosopttemplate/presentation/main/MainViewModel.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/main/MainViewModel.kt
@@ -1,15 +1,6 @@
 package org.sopt.dosopttemplate.presentation.main
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import org.sopt.dosopttemplate.data.UserInfo
 
 class MainViewModel : ViewModel() {
-    private val _user: MutableLiveData<UserInfo> = MutableLiveData()
-    val user : LiveData<UserInfo> = _user
-
-    fun setUserInfo(userInfo: UserInfo) {
-        _user.value = userInfo
-    }
 }
