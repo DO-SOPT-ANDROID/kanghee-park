e2a4b1c128e1bb194969c45cafd35aea21a82bf2
Hailey <91793891+stellar-halo@users.noreply.github.com>
2023-11-10 14:04:10 +0900

    [fix] 반복 코드 제거에 따른 data class 생성

--- app/src/main/java/org/sopt/dosopttemplate/presentation/myPage/MyPageFragment.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/myPage/MyPageFragment.kt
@@ -5,27 +5,13 @@ import android.view.View
 import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
 import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.data.DefaultData
 import org.sopt.dosopttemplate.data.UserInfo
 import org.sopt.dosopttemplate.databinding.FragmentMyPageBinding
 import org.sopt.dosopttemplate.util.binding.BindingFragment
 
 class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
     private val viewModel by viewModels<MyPageViewModel>()
-    private var _id: String? = null
-    private val id
-        get() = _id ?: ""
-
-    private var _password: String? = null
-    private val password
-        get() = _password ?: ""
-
-    private var _nickname: String? = null
-    private val nickname
-        get() = _nickname ?: ""
-
-    private var _mbti: String? = null
-    private val mbti
-        get() = _mbti ?: ""
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -35,15 +21,20 @@ class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_
 
     private fun getBundleArgs() {
         arguments ?: return
-        _id = arguments?.getString(ARGS_ID)
-        _nickname = arguments?.getString(ARGS_NICKNAME)
-        _mbti = arguments?.getString(ARGS_MBTI)
-        viewModel.setUserInfo(UserInfo(id, password, nickname, mbti))
+        val id = createDefaultData(ARGS_ID)
+        val password = createDefaultData(ARGS_PASSWORD)
+        val nickname = createDefaultData(ARGS_NICKNAME)
+        val mbti = createDefaultData(ARGS_MBTI)
+        viewModel.setUserInfo(UserInfo(id.value, password.value, nickname.value, mbti.value))
+    }
+
+    private fun createDefaultData(key: String): DefaultData {
+        return DefaultData(arguments?.getString(key))
     }
 
     companion object {
         private const val ARGS_ID = "ID"
-        private const val ARGS_PASSWORD = "PASSWORd"
+        private const val ARGS_PASSWORD = "PASSWORD"
         private const val ARGS_NICKNAME = "NICKNAME"
         private const val ARGS_MBTI = "MBTI"
 
