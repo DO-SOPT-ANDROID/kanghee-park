dbf030b9725606f934fc9a2b21f32642167e31c4
river99 <qkrrkd42@naver.com>
2023-11-17 07:22:14 +0900

    #8[feat] 버튼 클릭에 따른 서버 연결과 상태 변화 observe로 사용자에게 맞는 toast 띄우기

--- app/src/main/java/org/sopt/dosopttemplate/presentation/login/LoginActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/login/LoginActivity.kt
@@ -8,15 +8,12 @@ import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.dosopttemplate.R
 import org.sopt.dosopttemplate.databinding.ActivityLoginBinding
-import org.sopt.dosopttemplate.presentation.login.LoginViewModel.Companion.MEET_CRITERIA
-import org.sopt.dosopttemplate.presentation.login.LoginViewModel.Companion.NOT_MEET_CRITERIA
-import org.sopt.dosopttemplate.presentation.login.LoginViewModel.Companion.NOT_YET_SIGN_UP
+import org.sopt.dosopttemplate.domain.model.InputStatus
 import org.sopt.dosopttemplate.presentation.main.MainActivity
 import org.sopt.dosopttemplate.presentation.signUp.SignUpActivity
 import org.sopt.dosopttemplate.presentation.signUp.SignUpActivity.Companion.USER
 import org.sopt.dosopttemplate.util.binding.BindingActivity
 import org.sopt.dosopttemplate.util.hideKeyboard
-import org.sopt.dosopttemplate.util.snackBar
 import org.sopt.dosopttemplate.util.toast
 
 @AndroidEntryPoint
@@ -27,12 +24,13 @@ class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_lo
         binding.viewModel = viewModel
 
         initOnClickListener()
+        initObserver()
     }
 
     private fun initOnClickListener() {
         with(binding) {
             btnLoginLogin.setOnClickListener {
-                login()
+                viewModel?.postSignIn()
             }
             btnLoginSignUp.setOnClickListener {
                 moveToSignUp()
@@ -43,18 +41,6 @@ class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_lo
         }
     }
 
-    private fun login() {
-        when (viewModel.checkUserSignUp()) {
-            MEET_CRITERIA -> {
-                toast(LOGIN_SUCCEED)
-                moveToMain()
-            }
-
-            NOT_MEET_CRITERIA -> snackBar(binding.root, LOGIN_FAILED)
-            NOT_YET_SIGN_UP -> snackBar(binding.root, NON_MEMBER)
-        }
-    }
-
     private fun moveToSignUp() {
         val intentToSignUp = Intent(this, SignUpActivity::class.java)
         startActivity(intentToSignUp)
@@ -62,16 +48,48 @@ class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_lo
 
     private fun moveToMain() {
         val intentToMain = Intent(this, MainActivity::class.java).apply {
-            putExtra(USER, viewModel.user)
+            putExtra(USER, viewModel.loginUser.value)
             addFlags(FLAG_ACTIVITY_CLEAR_TASK or FLAG_ACTIVITY_NEW_TASK)
         }
         startActivity(intentToMain)
         finish()
     }
 
+    private fun initObserver() {
+        initLoginUserObserver()
+        initLoginStateObserver()
+        initFieldObserver()
+    }
+
+    private fun initLoginUserObserver() {
+        viewModel.loginUser.observe(this) {
+            if (viewModel.signInState.value == InputStatus.RIGHTINPUT) login()
+            else toast(LOGIN_FAILED)
+        }
+    }
+
+    private fun login() {
+        toast(LOGIN_SUCCEED + viewModel.loginUser.value?.id.toString())
+        moveToMain()
+    }
+
+    private fun initLoginStateObserver() {
+        viewModel.signInState.observe(this) { success ->
+            if (success == InputStatus.WRONGINPUT) toast(LOGIN_FAILED)
+        }
+    }
+
+    private fun initFieldObserver() {
+        viewModel.id.observe(this) {
+            viewModel.changeSignInStateToEditing()
+        }
+        viewModel.password.observe(this) {
+            viewModel.changeSignInStateToEditing()
+        }
+    }
+
     companion object {
-        const val LOGIN_SUCCEED = "로그인 성공"
+        const val LOGIN_SUCCEED = "로그인 성공 회원님의 id는 "
         const val LOGIN_FAILED = "로그인 실패. 비밀번호와 아이디를 다시 확인해주세요."
-        const val NON_MEMBER = "회원이 아닙니다. 회원가입을 진행해주세요."
     }
 }
