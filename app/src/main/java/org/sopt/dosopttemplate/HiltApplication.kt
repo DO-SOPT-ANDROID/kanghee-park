7f79cad9a9fc5af03581ee3244d28ff063908f80
river99 <qkrrkd42@naver.com>
2023-11-17 05:15:05 +0900

    #8[add] application 생성 및 hilt annotation

---
+++ app/src/main/java/org/sopt/dosopttemplate/HiltApplication.kt
@@ -0,0 +1,17 @@
+package org.sopt.dosopttemplate
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class HiltApplication:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
