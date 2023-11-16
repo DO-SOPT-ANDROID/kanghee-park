a26cacd0a57bef0d38cd1d33ca2355a1ed2c194b
river99 <qkrrkd42@naver.com>
2023-11-17 05:43:28 +0900

    #8[feat] postSignIn Service 추가

--- app/src/main/java/org/sopt/dosopttemplate/data/service/AuthService.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/service/AuthService.kt
@@ -1,12 +1,20 @@
 package org.sopt.dosopttemplate.data.service
 
-import org.sopt.dosopttemplate.data.entity.MemberRequest
+import org.sopt.dosopttemplate.data.entity.request.SignInRequest
+import org.sopt.dosopttemplate.data.entity.request.SignUpRequest
+import org.sopt.dosopttemplate.data.entity.response.SignInResponse
+import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthService {
     @POST("api/v1/members")
     suspend fun postSignUp(
-        @Body body: MemberRequest
+        @Body body: SignUpRequest
     )
+
+    @POST("api/v1/members/sign-in")
+    suspend fun postSignIn(
+        @Body body: SignInRequest
+    ) : Call<SignInResponse>
 }
